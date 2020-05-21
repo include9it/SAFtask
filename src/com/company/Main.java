56ce7d776a307cd017661b6403a2ef3255155c2e
Eduards <include9it@gmail.com>
2020-05-22 00:42:29 +0300

    first refactor

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,10 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            System.out.println("1");
             SimpleHttpServer.start();
-            System.out.println("2");
-
         } catch (IOException throwables) {
             throwables.printStackTrace();
         }
